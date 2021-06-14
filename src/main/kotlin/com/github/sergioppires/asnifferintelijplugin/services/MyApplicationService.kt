73421824a5c738ed6013881688fb002e70150115
GitHub Action <action@github.com>
2021-06-14 23:13:31 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/sergioppires/asnifferintelijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sergioppires.asnifferintelijplugin.services
+
+import com.github.sergioppires.asnifferintelijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
